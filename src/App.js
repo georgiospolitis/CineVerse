18ae1985a7219fc51ac2f552661975d8b4f65c49
DESKTOP-K02ELGS\gplts <gpolitis@devio.gr>
2023-07-13 12:17:23 +0300

    changes

--- src/App.js
+++ src/App.js
@@ -1,21 +1,32 @@
-import {useEffect} from 'react';
+import {useEffect, useState} from 'react';
 import './App.css';
 import cineverseLogo from './cineverse.svg';
 import searchIcon from './search.svg';
+import MovieCard from './MovieCard';
 
 const API_URL = 'http://www.omdbapi.com?apikey=94334455';
 
+const movie1 = {
+  "Title": "The Fast and the Furious: Tokyo Drift",
+  "Year": "2006",
+  "imdbID": "tt0463985",
+  "Type": "movie",
+  "Poster": "https://m.media-amazon.com/images/M/MV5BMTQ2NTMxODEyNV5BMl5BanBnXkFtZTcwMDgxMjA0MQ@@._V1_SX300.jpg"
+}
+
 const App = () => {
+
+  const [movies, setMovies] = useState([]);
 
   const searchMovies = async (title) => {
     const response = await fetch(`${API_URL}&s=${title}`);
     const data = await response.json();
 
-    console.log(data.Search);
+    setMovies(data.Search);
   }
 
   useEffect( () => {
-    searchMovies('Fast and Furious');
+    searchMovies('Spiderman');
   }, [] );
   
   return(
@@ -26,6 +37,38 @@ const App = () => {
         <h1>CineVerse</h1>
         <img className='cineverse-logo' src={cineverseLogo} alt='CineVerse Logo' />
       </div>
+
+      <div className='search'>
+        <input
+          placeholder='Αναζήτηση ταινιών'
+          value="Superman"
+          onChange={() => {}}
+        />
+        <img 
+          src={searchIcon} 
+          alt='search'
+          onClick={() =>{}}
+        />
+      </div>
+
+      {
+        movies?.length > 0
+        ? //if
+        (
+          <div className='container'>
+          {movies.map((movie) => (
+            <MovieCard movie={movie}></MovieCard>
+          ))}
+        </div>
+        ) 
+        : //else
+        (
+          <div className='empty'>
+            <h2>No movies found!</h2>
+          </div>
+        )
+      }
+      
 
     </div>
   );
